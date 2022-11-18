a187a27a4dfc2a962b8fa2ea2a601613b9b20eed
gMartins <45578391+psytins@users.noreply.github.com>
2022-11-18 19:13:01 +0000

    first design flow

--- src/Forms/GameMenuForm.java
+++ src/Forms/GameMenuForm.java
@@ -5,16 +5,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class GameMenuForm {
-    private JButton testButton;
     public JPanel mainPanel;
+    private JPanel centerPanel;
+    private JPanel topPanel;
+    private JPanel bottomPanel;
+    private JPanel leftPanel;
+    private JPanel rightPanel;
 
     public GameMenuForm() {
 
-        testButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                JOptionPane.showMessageDialog(null, "Hello World!");
-            }
-        });
     }
 }
