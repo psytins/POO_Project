530201c78e1a234a8fcf223014ac1b092eb6841c
João Paulo <joaop3152@gmail.com>
2022-11-18 20:13:27 +0000

    Game and Player Classes added

---
+++ src/Classes/Game.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class Game {
+    private Player playerOne;
+
+    private Player playerTwo;
+
+    private int scorePlayerOne;
+
+    private int scorePlayerTwo;
+
+    private int win;
+
+    private int grid;
+
+    private int gameOptions;
+}
---
+++ src/Classes/Player.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class Player {
+
+    //aqui no username ver o data type adequado!!
+    //varchar(50)  ???
+    private char username;
+
+    private int score;
+
+}
